14a5cb811d3a655795c70bca5243d05e267c4a54
TomFong <tom.fonglamhang@gmail.com>
2021-05-06 10:19:10 +0800

    first commit

---
+++ LdapApp/Const.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LdapApp
+{
+    public static class Const
+    {
+        public static readonly string LDAP_SERVER_ADDRESS = Environment.GetEnvironmentVariable("LDAP_SERVER_ADDRESS");
+        public static readonly string LDAP_SERVER_PORT = Environment.GetEnvironmentVariable("LDAP_SERVER_PORT");
+        public static readonly string LDAP_SERVER_PORT_SSL = Environment.GetEnvironmentVariable("LDAP_SERVER_PORT_SSL");
+    }
+}
---
+++ LdapApp/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LdapApp.Controllers
+{
+    [Route("api/[controller]")] // api/auth
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult TestRun()
+        {
+            return Ok(String.Format("Ldap URL => LDAP://{0}:{1}", Const.LDAP_SERVER_ADDRESS, Const.LDAP_SERVER_PORT));
+        }
+    }
+}
